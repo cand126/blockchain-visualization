08f5e6f3c26f5d6e05f45758a4294adc65ebcf1d
馬韶均 <justwhenithought@gmail.com>
2017-10-16 13:00:28 +0200

    add initUser and initTransaction

--- source/scripts/visualizer.js
+++ source/scripts/visualizer.js
@@ -5,26 +5,42 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera();
 var renderer = new THREE.WebGLRenderer();
 
-var geometry = new THREE.BoxGeometry(2, 2, 0);
-var material = new THREE.MeshBasicMaterial({ color: 0xc3bfb5, transparent: true, opacity: 0.5 });
-var cube = new THREE.Mesh(geometry, material);
+// blocks
+var blockGeometry = new THREE.BoxGeometry(2, 2, 0);
+var material = new THREE.MeshBasicMaterial({
+    color: 0xc3bfb5,
+    transparent: true,
+    opacity: 0.5
+});
+// var cube = new THREE.Mesh(geometry, material);
 
 var blockNumber = 5;
 
+// transactions
+var transactionNumber = 7;
+var transactionArray = [];
+var transactionGeometry = new THREE.BoxGeometry(15, 2.5, 0);
+var transactionMaterial = new THREE.MeshBasicMaterial({
+    color: 0xc3bfb5,
+    transparent: true,
+    opacity: 0.5
+});
 
-var transactionNumber = 10;
-
-
-
-
-var userNumber = 5;
-
-
+// users
+var userNumber = 20;
+var userArray = [];
+var colorArray = [
+    '#FF3030',
+    '#ADFF2F',
+    '#BF3EFF',
+    '#008B8B',
+    '#CDC1C5'
+];
 
 var texture = new THREE.ImageUtils.loadTexture('../images/ic_account_circle_white_24dp_2x.png');
 
 function initRenderer() {
-    renderer.setClearColor(0xffffff, 1);
+    renderer.setClearColor(0xFFFFFF, 1);
     renderer.setSize($('#canvas-container').width(), $('#canvas-container').height());
     $('#canvas-container').append(renderer.domElement);
 }
@@ -34,7 +50,7 @@ function initCamera() {
     camera.aspect = $('#canvas-container').width() / $('#canvas-container').height();
     camera.near = 0.1;
     camera.far = 1000;
-    camera.position.z = 10;
+    camera.position.z = 50;
     camera.updateProjectionMatrix();
     // camera.position.set(0, 0, 100);
     // camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -42,7 +58,43 @@ function initCamera() {
 
 function initScene() {
     // cube.position.x = 10;
-    scene.add(cube);
+    // scene.add(cube);
+
+    transactionArray.forEach(function(transaction, index) {
+        transaction.position.x = 80;
+        transaction.position.y = -30 + index * 5;
+        scene.add(transaction);
+    }, this);
+}
+
+function initUser() {
+    for (var i = 0; i < userNumber; i++) {
+        var userData = {};
+        userData.address = 'address';
+        // test colors (5 users)
+        if (i < 5) {
+            userData.color = colorArray[i];
+        } else {
+            userData.color = '#CAE1FF';
+        }
+        userArray.push(userData);
+    }
+
+    userArray.forEach(function (user) {
+        $('#user-area').append(
+            '<div class="row">' +
+            '<div class="col-sm-4"><img class="img-fluid" style="background-color: ' + user.color + '" src="../images/ic_account_circle_white_24dp_2x.png"></div>' +
+            '<div class="col-sm-8">' + user.address + '</div>' +
+            '</div>'
+        );
+    }, this);
+}
+
+function initTransaction() {
+    for (var i = 0; i < transactionNumber; i++) {
+        var transaction = new THREE.Mesh(transactionGeometry, transactionMaterial);
+        transactionArray.push(transaction);
+    }
 }
 
 function update() {
@@ -58,7 +110,8 @@ function animate() {
 $(document).ready(function () {
     initRenderer();
     initCamera();
-    initScene();
+    initUser();
+    initTransaction();
+    initScene();    
     animate();
-    console.log(camera);
 });
