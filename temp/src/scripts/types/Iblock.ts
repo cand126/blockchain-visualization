ac15f4d384949deeb4e0699c1efab1959818874a
Shao-Chun Ma <justwhenithought@gmail.com>
2017-11-30 15:24:08 +0100

    restructre

---
+++ temp/src/scripts/types/Iblock.ts
@@ -0,0 +1,38 @@
+import { Itransaction } from "./Itransaction";
+
+/**
+ * @interface
+ * @description a data type for blocks
+ * @property {string} id the id of the block
+ * @property {string} type is always "block", to indicate this object is a block
+ * @property {Date} timestamp the timestamp of the block, should be the time when the block is created
+ * @property {string} miner the id of the agent who mined the block
+ * @property {string[]} previous the array of id of the blocks that are the previous blocks
+ * @property {string[]} next the array of id of the blocks that are the next blocks
+ * @property {Itransaction[]} transactions the array of the transactions which is contained in the block, @see {@link Itransaction}
+ * @example
+ * {
+ *     id: ,
+ *     type: "block",
+ *     timestamp: ,
+ *     miner: ,
+ *     previous: ,
+ *     transactions: [
+ *         transaction1,
+ *         transaction2,
+ *         transaction3,
+ *         ...
+ *     ]
+ * }
+ */
+interface Iblock {
+    readonly id: string;
+    readonly type: string;
+    readonly timestamp: Date;
+    readonly miner: string;
+    previous: string[];
+    next: string[];
+    readonly transactions: Itransaction[];
+}
+
+export { Iblock };
---
+++ temp/src/scripts/types/Itransaction.ts
@@ -0,0 +1,29 @@
+import { Icontent } from "./Icontent";
+
+/**
+ * @interface
+ * @description a data type for transactions
+ * @property {string} id the id of the transaction
+ * @property {string} type is always "transaction", to indicate this object is a transaction
+ * @property {Date} timestamp the timestamp of the transaction, should be the time when the trasaction is created
+ * @property {Icontent} content the content of the transaction, @see {@link Icontent}
+ * @example
+ * {
+ *     id: ,
+ *     type: "transaction",
+ *     timestamp: ,
+ *     content: {
+ *         from: ,
+ *         to: ,
+ *         message:
+ *     }
+ * }
+ */
+interface Itransaction {
+    readonly id: string;
+    readonly type: string;
+    readonly timestamp: Date;
+    readonly content: Icontent;
+}
+
+export { Itransaction };
