b2c4fd6125126be3b37e99c7b4c601782effe996
Shao-Chun Ma <justwhenithought@gmail.com>
2018-01-10 00:13:27 +0100

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { VisualizerComponent } from './visualizer/visualizer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VisualizerComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
