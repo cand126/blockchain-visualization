199295b9b36fce5b577999bfd4260cffc3692e49
Shao-Chun Ma <justwhenithought@gmail.com>
2018-01-16 16:58:35 +0100

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VisualizerComponent } from './visualizer/visualizer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VisualizerComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
