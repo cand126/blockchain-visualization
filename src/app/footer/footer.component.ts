650d846475f9180306406756fc60143d916ca680
Shao-Chun Ma <justwhenithought@gmail.com>
2018-01-16 17:25:35 +0100

    add timer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
+  timer; // reference for the timer
+  counter = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.timer = setInterval(() => {
+      this.counter++;
+    }, 1000);
   }
 
+  ngOnDestroy() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+    }
+  }
 }
